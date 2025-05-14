263fc2173c5ca72d0b7fa87442064bed3effeec2
shravyasri28 <reddipallishravya01@gmail.com>
2025-05-14 23:25:16 +0530

    Create Datatypes.java

---
+++ Datatypes.java
@@ -0,0 +1,15 @@
+public main Datatypes{
+  public static void main(String[] args){
+    int n1=849734674;
+    byte n2=126;
+    short n3=7474977;
+    long n4=764796796967l;
+
+    double n5=98.89658584949;
+    float n6=809.7788f;
+
+    char n7='s';
+
+    boolean n8=true;
+  }
+}
