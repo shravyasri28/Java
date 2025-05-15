9cf9a293c671cebcfb6a9d89a7e90850ee4ed042
shravyasri28 <reddipallishravya01@gmail.com>
2025-05-15 21:36:56 +0530

    Create TypeCasting.java

---
+++ TypeCasting.java
@@ -0,0 +1,15 @@
+class TypeCasting{
+  public static void main(String[] args){
+    byte n1=126;
+    int n2=n1;
+    System.out.println(n2);
+    int n3=56;
+    byte n4=(byte)n3;
+    System.out.println(n4);
+    int n5=276;
+    byte n6=(byte)n5;
+    System.out.println(n6);
+    float n7=78.8987f;
+    int n8=(int)n7;
+    System.out.println(n8);}
+}
