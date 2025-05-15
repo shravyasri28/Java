4bfba4f517b50c0d7bd19bced0eafc8f8dc23910
shravyasri28 <reddipallishravya01@gmail.com>
2025-05-15 20:32:09 +0530

    Create Literals.java

---
+++ Literals.java
@@ -0,0 +1,13 @@
+public class Literals{
+  public static void main(){
+    int n1=0b1011;
+    int n2=0x7E;
+    double n3=70;
+    char n4='s';
+    System.out.println(n1);
+    System.out.println(n2);
+    System.out.println(n3);
+    n4++;
+    System.out.println(n4);
+  }
+}
