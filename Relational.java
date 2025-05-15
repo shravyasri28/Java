d9c5a26d985d25d7df2e14637c8218c4a7a3079b
shravyasri28 <reddipallishravya01@gmail.com>
2025-05-15 22:42:44 +0530

    Create Relational.java

---
+++ Relational.java
@@ -0,0 +1,18 @@
+class Relational{
+  public static void main(String[] args){
+    int n1=9;
+    int n2=7;
+    boolean n3=n1<n2;
+    System.out.println(n3);
+    boolean n4=n1>n2;
+    System.out.println(n4);
+    int n5=8;
+    int n6=7;
+    boolean n7=n5<=n6;
+    System.out.println(n7);
+    int n8=4;
+    int n9=4;
+    boolean n10=n8==n9;
+    System.out.println(n10);
+  }
+}
