a8fb9a443599b0e5f526342976d95cc1440fec69
shravyasri28 <reddipallishravya01@gmail.com>
2025-05-15 21:44:52 +0530

    Update Literals.java

--- Literals.java
+++ Literals.java
@@ -4,7 +4,7 @@ public class Literals{
     int n2=0x7E;
     double n3=70;
     char n4='s';
-    int n5=100_00_000_000;
+    int n5=100_00_000;
     System.out.println(n1);
     System.out.println(n2);
     System.out.println(n3);
