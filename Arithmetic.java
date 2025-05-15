41164a056e2a4c45a703931e20315da335cbf27c
shravyasri28 <reddipallishravya01@gmail.com>
2025-05-15 22:27:19 +0530

    Create Arithmetic.java

---
+++ Arithmetic.java
@@ -0,0 +1,19 @@
+class Arithmetic{
+  public static void main(String[] args){
+    int n1=9;
+    int n2=5;
+    int n3=n1+n2;
+    int n4=n1-n2;
+    int n5=n1*n2;
+    int n6=n1/n2;
+    int n7=n1%n2;
+    System.out.println(n3);
+    System.out.println(n4);
+    System.out.println(n5);
+    System.out.println(n6);
+    System.out.println(n7);
+    int n8=n1++;
+    int n9=++n1;
+    System.out.println(n8);
+    System.out.println(n9);}
+}
