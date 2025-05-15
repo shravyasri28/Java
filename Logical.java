a141d22d213296168dbf85312fdd0678034ceeee
shravyasri28 <reddipallishravya01@gmail.com>
2025-05-15 22:58:49 +0530

    Create Logical.java

---
+++ Logical.java
@@ -0,0 +1,14 @@
+class Logical{
+  public static void main(String[] args);
+  int n1=9;
+  int n2=5;
+  int n3=6;
+  int n4=7;
+  boolean n5=n1>n2 && n3<n4;
+  System.out.println(n5);
+  boolean n6=n1<n2 || n3<n4;
+  System.out.println(n6);
+  boolean n7=n1>n2;
+  System.out.println(n7);
+}
+}
