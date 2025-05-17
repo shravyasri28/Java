9c5e6d85e88a97205e4c4a3454669306ec87eaf2
shravyasri28 <reddipallishravya01@gmail.com>
2025-05-17 09:11:23 +0530

    Update switchNew.java

--- switchNew.java
+++ switchNew.java
@@ -2,18 +2,19 @@ class switchNew{
   public static void main(String[] args){
     String day="Tuesday";
     String res="";
-    switch(day){
-      case "Monday","Tuesday" -> return res="6am";
-      case "Wednesday","Thursday","Friday" ->return res="7am";
-      default -> res="10am";  
+    res=switch(day){
+      case "Monday","Tuesday" -> "6am";
+      case "Wednesday","Thursday","Friday" ->"7am";
+      default -> "10am";  
     };
     System.out.println(res);
-    String day="Friday";
-    String res="";
-    res=switch(day){
+    String day1="Friday";
+    String res1="";
+    res1=switch(day1){
         case "Monday","Tuesday" :yield "6am";
       case "Wednesday","Thursday","Friday" :yield "7am";
       default :yield "10am";  
     };
-    System.out.println(res);
+    System.out.println(res1);
   }
+}
