654efacb88ae968c75b58de606132b0f03fa0e76
shravyasri28 <reddipallishravya01@gmail.com>
2025-05-19 20:34:21 +0530

    Create methods.java

---
+++ methods.java
@@ -0,0 +1,19 @@
+class Applications{
+  public void playMusic(){
+    System.out.println("Music is Playing...");
+  }
+  public String getMeAPen(int cost){
+    if(cost>=8)
+      return "Pen Delivered";
+    else
+      return "Not Delivered";
+  }
+}
+class Methods{
+  public static void main(String[] args){
+    Application obj=new Application();
+    obj.playMusic();
+    String pen=obj.getMeAPen(10);
+    System.out.println(pen);
+  }
+}
