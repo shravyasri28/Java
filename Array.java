189568987a22a38bbdb2d506013ea391bdbe93cf
shravyasri28 <reddipallishravya01@gmail.com>
2025-05-22 22:01:32 +0530

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,16 @@
+class Array{
+  int arr[]=new int[10];
+  int arr[0]=10;
+  int arr[1]=20;
+  int arr[2]=30;
+  int arr[3]=40;
+  int arr[4]=50;
+  int arr[5]=70;
+  int arr[6]=80;
+  int arr[7]=90;
+  int arr[8]=100;
+  int arr[9]=110;
+  for(int i=0;i<=10;i++){
+  System.out.println(arr[i]);
+}
+}
