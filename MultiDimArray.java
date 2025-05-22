8b662e5aac7384bf679d218e2f75d36fe4998094
shravyasri28 <reddipallishravya01@gmail.com>
2025-05-22 22:47:15 +0530

    Update MultiDimArray.java

--- MultiDimArray.java
+++ MultiDimArray.java
@@ -2,17 +2,17 @@ class MultiDimArray{
   public static void main(String[] args){
     int arr[][]=new int[3][4];
     arr[0][0]=12;
-    arr[0][1]=12;
-    arr[0][2]=12;
-    arr[0][3]=12;
-    arr[1][0]=12;
-    arr[1][1]=12;
-    arr[1][2]=12;
-    arr[1][3]=12;
-    arr[2][0]=12;
-    arr[2][1]=12;
-    arr[2][2]=12;
-    arr[2][3]=12;
+    arr[0][1]=87;
+    arr[0][2]=92;
+    arr[0][3]=24;
+    arr[1][0]=09;
+    arr[1][1]=29;
+    arr[1][2]=93;
+    arr[1][3]=18;
+    arr[2][0]=56;
+    arr[2][1]=89;
+    arr[2][2]=90;
+    arr[2][3]=32;
     for(int i=0;i<=2;i++){
       for(int j=0;j<=3;j++){
         System.out.print(arr[i][j]);
@@ -22,7 +22,7 @@ class MultiDimArray{
     int arr1[][]=new int[3][4];
     for(int i=0;i<=2;i++){
       for(int j=0;j<=3;j++){
-        arr1[i][j]=(int)((Math.random)*10);
+        arr1[i][j]=(int)((Math.random()*10);
       }
     }
     for(int i=0;i<=2;i++){
