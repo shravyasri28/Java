fb12fadc209da4c7ee72413dafa39bba2a700dae
shravyasri28 <reddipallishravya01@gmail.com>
2025-05-22 22:11:22 +0530

    Update Array.java

--- Array.java
+++ Array.java
@@ -1,16 +1,18 @@
 class Array{
+  public static void main(String[] args){
   int arr[]=new int[10];
-  int arr[0]=10;
-  int arr[1]=20;
-  int arr[2]=30;
-  int arr[3]=40;
-  int arr[4]=50;
-  int arr[5]=70;
-  int arr[6]=80;
-  int arr[7]=90;
-  int arr[8]=100;
-  int arr[9]=110;
-  for(int i=0;i<=10;i++){
+  arr[0]=10;
+  arr[1]=20;
+  arr[2]=30;
+  arr[3]=40;
+  arr[4]=50;
+  arr[5]=70;
+  arr[6]=80;
+  arr[7]=90;
+  arr[8]=100;
+  arr[9]=110;
+  for(int i=0;i<=9;i++){
   System.out.println(arr[i]);
+}
 }
 }
